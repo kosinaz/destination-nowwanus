7589c14a2cee67e39fc0ad21bf339163b24def9c
kosinaz <kosinaz@gmail.com>
2020-04-24 09:55:48 +0200

    format info

--- src/infoScene.js
+++ src/infoScene.js
@@ -28,44 +28,35 @@ export default class InfoScene extends Phaser.Scene {
     bg.setDepth(-3);
     bg.setAlpha(0.10);
     this.add.text(16, 16,
-        ` In 2026 New Horizons will fly by its second Kuiper Belt Object,
+        `  In 2026 New Horizons will fly by its second Kuiper Belt Object,
 860626 Nowwanus, provisional designation KBO 2021 KD19.
 
- You have to navigate through the asteroid fields using the asteroid location
+  You have to navigate through the asteroid fields using the asteroid location
 predictions based on the space dust analytics of the VBSDC subsystem.
 
- You also have take valuable pictures of the asteroids with the Ralph 
+  You also have take valuable pictures of the asteroids with the Ralph 
 to maintain our funding and allow us to invest in researches related 
 to the operation of the probe.
 
- Better ways to use the RTG will grant us more power to take pictures, 
-better image compression for the Ralph will give us bigger pictures,
-better analytics for the VBSDC will make us notice the asteroids earlier,
-providing us more time to find a way between them.`, {
+  Better ways to use the RTG will grant us more power to take pictures, ` +
+`better image compression for the Ralph
+will give us bigger pictures, and better analytics for the VBSDC ` +
+`will let us notice the asteroid fields earlier,
+providing us more time to find a way through them.`, {
           fontSize: '14px',
           fontFamily: 'font2',
           color: 'lightgray',
+          lineSpacing: 8,
         }).setOrigin(0);
-    this.add.text(512, 268,
-        `Art
-
-Free space shooter game GUI, Free space shooter game objects,` +
-` 2d fantasy fairy free character sprite,
-Free orc ogre and goblin chibi 2d game sprites by CraftPix` +
-` and original content by Zoltan Kosina
-
-Font
-
-Ethnocentric, Recharge by Typodermic Fonts
-
-Music
-
-Endless Cyber Runner, Runaway Technology, 4.2 Light Years` +
-` by Eric Matyas www.soundimage.org`, {
+    this.add.text(512, 374,
+        `Art by CraftPix and Zoltan Kosina
+Fonts by Typodermic Fonts
+Music by Eric Matyas www.soundimage.org`, {
           fontSize: '14px',
           fontFamily: 'font2',
           color: 'lightgray',
           align: 'center',
+          lineSpacing: 18,
         }).setOrigin(0.5, 0);
     const newhorizons = this.add.image(0, 0, 'sprites', 'newhorizonstitle');
     const rtgline = this.add.line(0, 0, 0, 0, 25, -25, 0xffffff)
@@ -89,7 +80,7 @@ Endless Cyber Runner, Runaway Technology, 4.2 Light Years` +
       fontFamily: 'font',
     }).setOrigin(0);
     const vbsdc = this.add.container(20, 70, [vbsdcline, vbsdctext]);
-    this.add.container(780, 130, [newhorizons, rtg, ralph, vbsdc]);
+    this.add.container(780, 120, [newhorizons, rtg, ralph, vbsdc]);
     this.cameras.main.on('camerafadeoutcomplete', () => {
       this.scene.start('MenuScene', data);
     });
